305cf3c91e14ea8913edefb68770bf238d6916ab
shahanavp <79835469+shahanavp@users.noreply.github.com>
2021-03-24 22:46:14 +0530

    co5

---
+++ CO5/CO5_1.py
@@ -0,0 +1,5 @@
+fh = open("Flower.txt")
+lst = list()
+for line in fh:
+    lst.append(line)
+print(lst)
---
+++ CO5/CO5_2.py
@@ -0,0 +1,12 @@
+newFile = open("States.txt","w")
+newFile.write("Goa \nKerala \nTamilnadu \nBangal\nPunjab")
+newFile.close()
+
+readFile = open("States.txt","r")
+lines = readFile.readlines()
+readFile.close()
+
+oddFile = open("oddcontent.txt","w")
+for i in range(0,len(lines),2):
+    oddFile.write(lines[i])
+oddFile.close()
